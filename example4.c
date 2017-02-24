71e4b110f61ce6e8543dcf22d0aace53af2e3a3f
Pushkar Pandey <pushkarpandey2196@gmail.com>
2017-02-25 02:07:33 +0530

    Update example4.c

--- example4.c
+++ example4.c
@@ -2,19 +2,25 @@
 #include <omp.h>
 int main()
 {
-	#pragma omp parallel sections
-	{
-   		#pragma omp section
-     		{
-       			printf("Hello World One\n");
-     		}
-		#pragma omp section
-     		{
-       			printf("Hello World Two\n");
-     		}
-		#pragma omp section
-     		{
-       			printf("Hello World Three\n");
-     		}
-	}
+	#pragma omp parallel sections num_threads(3)
+   	{
+     		#pragma omp section
+      		{
+        		int tid;
+        		tid=omp_get_thread_num();
+        		printf("X printed by thread with id=%d\n",tid);
+      		}
+     		#pragma omp section
+      		{
+        		int tid;
+        		tid=omp_get_thread_num();
+        		printf("Y printed by thread with id=%d\n",tid);
+      		}
+     		#pragma omp section
+      		{
+        		int tid;
+        		tid=omp_get_thread_num();
+        		printf("Z printed by thread with id=%d\n",tid);
+      		}
+   	}
 }
